89c9fb91690408c8e5104d12d3d41eedebbec617
Talles Souza <talles-swd@live.com>
2025-08-20 23:23:35 +0000

    feat: move production to Brazil South region
    - Change production location from East US to Brazil South
    - Avoid Container App Environment limits in East US
    - Reduce latency for Brazilian users
    - Brazil South has better quota availability

--- terraform/environments/production/variables.tf
+++ terraform/environments/production/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Região do Azure"
   type        = string
-  default     = "East US"
+  default     = "Brazil South"
 }
 
 variable "domain_name" {
