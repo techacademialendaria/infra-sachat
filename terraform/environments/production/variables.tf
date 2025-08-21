1efcdbebbac01dda95436ec0212a70f8cd9e3b9d
Talles Souza <talles-swd@live.com>
2025-08-21 00:16:42 +0000

    fix: update resource group name to 'rg-sachat-prod' for consistency

--- terraform/environments/production/variables.tf
+++ terraform/environments/production/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "Nome do Resource Group"
   type        = string
-  default     = "rg-sachat-production"
+  default     = "rg-sachat-prod"
 }
 
 variable "location" {
